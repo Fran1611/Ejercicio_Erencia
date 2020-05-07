2c8bba181539aa454624d8082f1a9c9e937749f2
Fran1611 <francisco.ibanez@correo.ucu.edu.uy>
2020-05-06 23:40:52 -0300

    update program, passenger, pool, single and driver

--- src/Library/Pool.cs
+++ src/Library/Pool.cs
@@ -4,24 +4,10 @@ namespace Herencia
 {
     public class Pool : Driver
     {
-        private int passengersCount;
-        public Pool(string name, string surname, int iD, string vehicle, string biography, int passengersCount):base(name,surname,iD,vehicle,biography)
+        
+        public Pool(string name, string surname, int iD, string vehicle, string biography, int passengersCount):base(name,surname,iD,vehicle,biography,passengersCount)
         {
             this.PassengersCount = passengersCount;
-        }
-
-        public int PassengersCount 
-        {
-            get{return passengersCount;}
-            
-            set
-            {
-                if (0<= value && value<=4)
-                {
-                    passengersCount = value;
-                }
-            }
-            
         }
     }
 } 
