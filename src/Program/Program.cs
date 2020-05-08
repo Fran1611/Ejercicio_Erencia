1bcf3f12b9bddb4b7edb4f2ac7aac99a37b8dfbe
ShadowGod001 <55005167+ShadowGod001@users.noreply.github.com>
2020-05-08 00:47:29 -0300

    Creacion de nuevo archivo FoundFace.cs
    Movi las fotos de program a library para que funcionara pero deberiamos encontrar una forma de llamarlas desde program
    
    Cree el nuevo archivo FoundFace donde estan los métodos DriverFace y PassengerFace para validar cuando mandar el tuit
    
    Deje comentado lo que habia antes en el program por si lo que hice esta todo mal asi seguimos a partir de lo comentado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -26,32 +26,36 @@ namespace Program
             conductor1.Califications(francisco,4);
             conductor1.Califications(alejandro,5);
 
-            string consumerKey = "g7rkPB5uI2xOqELAhlNrorSU4";
-            string consumerKeySecret = "8hOTyS71GrTH9Ool3rXykAJRY5AmgSPiy78b1wYUPcvfIzXeEc";
-            string accessTokenSecret = "675fHmUzeaPajtj3pO64w5xd3p9YI3kco7kSvKhzeEvYe";
-            string accessToken = "1396065818-8vnV9HJFW5ArcfFg2zE9hLA68CZYFXO8Cjv6o2E";
+           
 
             //Se debe pasar por parametro la API Key y el color del recuadro a dibujar 
             //en caso de encontrar una cara en la foto
             CognitiveFace cog = new CognitiveFace("620e818a46524ceb92628cde08068242", true);
-            cog.Recognize(@"images.jpg");
-            FoundFace(cog);
-            cog.Recognize(@"img.jpg");
-            FoundFace(cog);
             cog.Recognize(@"sonrisa.jpg");
-            FoundFace(cog);
-
-            var twitter = new TwitterImage(consumerKey, consumerKeySecret, accessToken, accessTokenSecret);
-            Console.WriteLine(twitter.PublishToTwitter($"Bienvenido {conductor1.Name}! El nuevo Conductor Pool de UCURide que llevará hasta {conductor1.PassengersCount} pasajeros Bio: {conductor1.Biography} ",@"sonrisa.jpg"));
+            Herencia.FoundFace.DriverFace(cog,conductor1.Name,conductor1.PassengersCount,conductor1.Biography);
+            //FoundFace(cog,conductor1.Name,conductor1.PassengersCount,conductor1.Biography);
+            cog.Recognize(@"images.jpg");
+            Herencia.FoundFace.PassengerFace(cog,valentina.Name,valentina.Surname,valentina.Id);
+            //cog.Recognize(@"sonrisa.jpg");
+            //FoundFace(cog);
+            
         }
-        static void FoundFace(CognitiveFace cog)
+        /*
+        static void FoundFace(CognitiveFace cog, string Name, int PassengersCount, string Biography)
         {
+            string consumerKey = "g7rkPB5uI2xOqELAhlNrorSU4";
+            string consumerKeySecret = "8hOTyS71GrTH9Ool3rXykAJRY5AmgSPiy78b1wYUPcvfIzXeEc";
+            string accessTokenSecret = "675fHmUzeaPajtj3pO64w5xd3p9YI3kco7kSvKhzeEvYe";
+            string accessToken = "1396065818-8vnV9HJFW5ArcfFg2zE9hLA68CZYFXO8Cjv6o2E";
+            var twitter = new TwitterImage(consumerKey, consumerKeySecret, accessToken, accessTokenSecret);
+
             if (cog.FaceFound)
             {
                 Console.WriteLine("Face Found!");
                 if (cog.SmileFound)
                 {
                     Console.WriteLine("Found a Smile :)");
+                    Console.WriteLine(twitter.PublishToTwitter($"Bienvenido {Name}! El nuevo Conductor Pool de UCURide que llevará hasta {PassengersCount} pasajeros Bio: {Biography} ",@"sonrisa.jpg"));
                 }
                 else
                 {
@@ -61,6 +65,7 @@ namespace Program
             else
                 Console.WriteLine("No Face Found");
         }
+        */
     
     
     }
